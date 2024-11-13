bee514a02a5d6a7f6786642e31acc80f26bc09d1
hackgenai2024 <ajit_nair@hotmail.com>
2024-11-13 17:15:43 -0600

    Sample -1
    Sample -1

--- server/utils.go
+++ server/utils.go
@@ -2,6 +2,9 @@ package server
 
 import (
 	"os"
+	"fmt"
+    "io/ioutil"
+    "net/http"
 )
 
 func GetEnv(key, fallback string) string {
@@ -9,4 +12,35 @@ func GetEnv(key, fallback string) string {
 		return value
 	}
 	return fallback
+}
+
+func getInfo() {
+	// Define the URL
+    url := "https://jsonplaceholder.typicode.com/posts/1"
+
+    // Create a new HTTP request
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        fmt.Println("Error creating request:", err)
+        return
+    }
+
+    // Send the request using the default HTTP client
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("Error making request:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    // Read the response body
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("Error reading response body:", err)
+        return
+    }
+
+    // Print the response body
+    fmt.Println(string(body))
 }
